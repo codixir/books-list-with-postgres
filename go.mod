07b1c4f5f9b71929caa6c98efc5eae385843f3ff
codixir <codixir@gmail.com>
2020-05-05 21:28:50 -0400

    update cors config

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module books-list
 
 require (
 	github.com/davecgh/go-spew v1.1.1
+	github.com/gorilla/handlers v1.4.2
 	github.com/gorilla/mux v1.7.0
 	github.com/lib/pq v1.0.0
 	github.com/subosito/gotenv v1.1.1
 )
+
+go 1.13
--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"books-list/controllers"
+	"books-list/driver"
+	"books-list/models"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
 
-	"books-list/controllers"
-	"books-list/driver"
-	"books-list/models"
-
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/subosito/gotenv"
 )
@@ -20,7 +20,6 @@ var db *sql.DB
 func init() {
 	gotenv.Load()
 }
-
 func main() {
 	db = driver.ConnectDB()
 	controller := controllers.Controller{}
@@ -34,5 +33,6 @@ func main() {
 	router.HandleFunc("/books/{id}", controller.RemoveBook(db)).Methods("DELETE")
 
 	fmt.Println("Server is running at port 8000")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"}),
+		handlers.AllowedOrigins([]string{"*"}))(router)))
 }
